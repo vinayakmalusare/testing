dcb343824b6f4b8862e0883d21241f491121b2d6
unknown <malusarevinayak01@gmail.com>
2025-04-07 10:25:39 +0530

    new SOP changes updated

--- src/test/java/org/example/stringDuplication.java
+++ src/test/java/org/example/stringDuplication.java
@@ -9,6 +9,7 @@ class GFG {
            for(int i =0;i<reverse.length();i++){
                ch = reverse.charAt(i);
                r= ch+r;
+               System.out.println("New String After reverse: " +r);
 
            }
            System.out.println(r);
